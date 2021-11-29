1141bfd0895f5e414cde33f4f870081e40997f27
longlivejosef <93947270+longlivejosef@users.noreply.github.com>
2021-11-29 14:10:47 -0500

    Add files via upload

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  // put setup code here
+  createCanvas(700, 700);
+}
+
+function draw() {
+  // put drawing code here
+  background(105, 186, 183);
+
+  noStroke();
+  fill(100);
+  rect(200, 100, 200, 100);
+
+  noStroke();
+  fill(150);
+  elipse(50, 50, 50, 50);
+}
